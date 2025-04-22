f7fe15d567cf5f6e9083128b5bbe4252d3fbbb87
kavindu-senarathne <senarathnalpsuk.21@uom.lk>
2025-04-22 17:50:04 +0530

    trying 6

--- kaggle_push.py
+++ kaggle_push.py
@@ -3,14 +3,14 @@ import os
 notebook_path = "dummy_notebook.ipynb"
 kaggle_username = os.environ['KAGGLE_USERNAME']
 # kernel_slug = f"{kaggle_username}/kaggle-git.ipynb"
-kernel_slug = "kavindu210588u/kaggle-git.ipynb"
+kernel_slug = "kavindu210588u/kaggle-and-git"
 
 
 # prepare kernel-metadata.json
 kernel_meta = f"""
 {{
   "id": "{kernel_slug}",
-  "title": "kaggle-git",
+  "title": "Kaggle and Git",
   "code_file": "{notebook_path}",
   "language": "python",
   "kernel_type": "notebook",
