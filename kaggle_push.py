12b36d38d4416f97c10ca3c83d6659ac8048f037
kavindu-senarathne <senarathnalpsuk.21@uom.lk>
2025-04-22 17:02:47 +0530

    adjustedthe title in kernel_meta

--- kaggle_push.py
+++ kaggle_push.py
@@ -3,14 +3,14 @@ import os
 notebook_path = "dummy_notebook.ipynb"
 kaggle_username = os.environ['KAGGLE_USERNAME']
 # kernel_slug = f"{kaggle_username}/kaggle-git.ipynb"
-kernel_slug = "kavindu210588u/kaggle-git.ipynb"
+kernel_slug = "kavindu210588u/kaggle-git"
 
 
 # prepare kernel-metadata.json
 kernel_meta = f"""
 {{
   "id": "{kernel_slug}",
-  "title": "Auto Pushed Notebook",
+  "title": "kaggle-git",
   "code_file": "{notebook_path}",
   "language": "python",
   "kernel_type": "notebook",
