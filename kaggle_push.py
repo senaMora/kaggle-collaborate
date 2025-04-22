cecdcf21761e97331a2accab8f892213135ee7b1
kavindu-senarathne <senarathnalpsuk.21@uom.lk>
2025-04-22 17:42:55 +0530

    trying 2

--- kaggle_push.py
+++ kaggle_push.py
@@ -10,7 +10,7 @@ kernel_slug = "kavindu210588u/kaggle-git.ipynb"
 kernel_meta = f"""
 {{
   "id": "{kernel_slug}",
-  "title": "kaggle-git.ipynb",
+  "title": "kaggle-git",
   "code_file": "{notebook_path}",
   "language": "python",
   "kernel_type": "notebook",
